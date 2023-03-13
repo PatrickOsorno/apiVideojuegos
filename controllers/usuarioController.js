c8e48cb787b1f0c8aac3a94fe79efe5cc206560b
Patrick Osorno <Patrick Osorno@LAPTOP-HP5C20QU>
2023-03-12 20:26:25 -0600

    12/03/2023

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 module.exports.get = async (req, res) => {
   let credenciales = req.body;
   res.json(
-    await prisma.usuario.findUnique({
+    await prisma.usuario.findFirst({
       where: { email: credenciales.email, password: credenciales.password },
     })
   );
